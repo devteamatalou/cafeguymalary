14a003067af15521995a150b4717ba20b65b1cb6
twigyf20 <twistawoodson@gmail.com>
2023-10-23 13:18:51 -0400

    test commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devteam/ataschool',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dfe76733f00125dcc5587d59276a1c9eff883bc8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devteam/ataschool' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dfe76733f00125dcc5587d59276a1c9eff883bc8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ views/admin/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Caféteria Guy Malary</title>
+    <link href="../../assets/css/styleLogin.css" rel="stylesheet">
+    <!-- cdn Icon bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <div class="container-login">
+        <div class="item">
+            <img src="../../assets/img/bgc1.png" alt="">
+        </div>
+        <div class="item">
+            <form action="">
+                <p class="title-form"> <span>Login</span> | Restaurant Guy Malary</p>
+                <div class="input-group">
+                    <span class="icon"><i class="bi bi-person"></i></span>
+                    <input type="text" placeholder="Ex: Joe*****" require>
+                </div>
+                <div class="input-group">
+                    <span class="icon"><i class="bi bi-lock"></i> </span>
+                    <input type="password" placeholder="Ex: 123*****">
+                </div>
+                <span>
+                    <button type="submit">Login</button>
+                    <a href="#">Create an account</a>
+                </span>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
