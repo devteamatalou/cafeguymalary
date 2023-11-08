38ea68bc6a9dd5c088e65bc2787e63ff68365995
twigyf20 <twistawoodson@gmail.com>
2023-11-08 12:35:05 -0500

    change database connection settings for online

--- src/config/Database.php
+++ src/config/Database.php
@@ -6,15 +6,15 @@ use PDO;
 
 class Database
 {
-	private  $SERVERNAME = 'localhost';
-	private  $USERNAME = 'root';
-	private  $PASSWORD = '';
+	private  $SERVERNAME = 'db5007325984.hosting-data.io';
+	private  $USERNAME = 'dbu3335240';
+	private  $PASSWORD = '@AanCafe@@2023@@';
 	protected  $conn = null;
 
 	public function __construct()
 	{
 		try {
-			$this->conn = new PDO("mysql:host=$this->SERVERNAME;dbname=airport_cafe", $this->USERNAME, $this->PASSWORD);
+			$this->conn = new PDO("mysql:host=$this->SERVERNAME;dbname=dbs6036076", $this->USERNAME, $this->PASSWORD);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (\PDOException $e) {
 			die('Erreur => ' . $e);
