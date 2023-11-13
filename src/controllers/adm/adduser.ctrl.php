a991aa83f8edcd8e563edb468cb949697e33e197
twigyf20 <twistawoodson@gmail.com>
2023-11-13 12:49:20 -0500

    commit

--- src/controllers/adm/adduser.ctrl.php
+++ src/controllers/adm/adduser.ctrl.php
@@ -14,7 +14,7 @@
 		$lname = str_replace(' ', '', htmlspecialchars($lname));
 		$username = str_replace(' ', '', htmlspecialchars($uname));
 		$auth = htmlspecialchars($default_pass);
-		$pswd_hash = password_hash($default_pass, PASSWORD_DEFAULT);
+		$pswd_hash = password_hash($auth, PASSWORD_DEFAULT);
 
 		if($adduser = $userDao->addUser($id_role, $fname, $lname, $username, $pswd_hash))
 			$response = ['status' => true, 'message' => 'User added successfully'];
