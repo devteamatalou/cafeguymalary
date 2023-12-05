e2a9d90910ec0a97360d4eda4333d7abb9328028
twigyf20 <twistawoodson@gmail.com>
2023-12-05 18:05:16 -0500

    add restriction for on Simple User

--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -6,39 +6,54 @@
 				<i class="bi bi-grid"></i>
 				<span>Dashboard</span>
 			</a>
-		</li><!-- End Dashboard Nav -->
+		</li>
+		<!-- End Dashboard Nav -->
 
 		<!-- Start setion user nav -->
-		<li class="nav-item">
-			<a class="nav-link collapsed" data-bs-target="#user-nav" data-bs-toggle="collapse" href="#">
-				<i class="bi bi-person"></i><span>Users</span><i class="bi bi-chevron-down ms-auto"></i>
-			</a>
-			<ul id="user-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/users.php"><i class="bi bi-circle"></i><span>List Users</span></a>
+		<?php
+		 if($_SESSION['admin']['id_role'] == 1):
+		?>
+				<li class="nav-item">
+					<a class="nav-link collapsed" data-bs-target="#user-nav" data-bs-toggle="collapse" href="#">
+						<i class="bi bi-person"></i><span>Users</span><i class="bi bi-chevron-down ms-auto"></i>
+					</a>
+					<ul id="user-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/users.php"><i class="bi bi-circle"></i><span>List Users</span></a>
+						</li>
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/delusers.php"><i class="bi bi-circle"></i><span>Deleted Users</span></a>
+						</li>
+					</ul>
 				</li>
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/delusers.php"><i class="bi bi-circle"></i><span>Deleted Users</span></a>
-				</li>
-			</ul>
-		</li><!-- End user Nav -->
+		<?php
+		 endif;
+		?>
+		<!-- End user Nav -->
 
-		<!-- Start section employe -->
-		<li class="nav-item">
-			<a class="nav-link collapsed" data-bs-target="#employe-nav" data-bs-toggle="collapse" href="#">
-				<i class="bi bi-person-workspace"></i><span>Employees</span><i class="bi bi-chevron-down ms-auto"></i>
-			</a>
-			<ul id="employe-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/employees.php"><i class="bi bi-circle"></i><span>List Employees</span></a>
+		<!-- Start section employee -->
+		<?php
+		 if($_SESSION['admin']['id_role'] == 1):
+		?>
+				<li class="nav-item">
+					<a class="nav-link collapsed" data-bs-target="#employe-nav" data-bs-toggle="collapse" href="#">
+						<i class="bi bi-person-workspace"></i><span>Employees</span><i class="bi bi-chevron-down ms-auto"></i>
+					</a>
+					<ul id="employe-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/employees.php"><i class="bi bi-circle"></i><span>List Employees</span></a>
+						</li>
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/delemployees.php"><i class="bi bi-circle"></i><span>Deleted Employees</span></a>
+						</li>
+					</ul>
 				</li>
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/delemployees.php"><i class="bi bi-circle"></i><span>Deleted Employees</span></a>
-				</li>
-			</ul>
-		</li><!-- End section employe  -->
+		<?php
+		 endif;
+		?>
+		<!-- End section employee  -->
 
-		<!-- Start section employe -->
+		<!-- Start section chekin -->
 		<li class="nav-item">
 			<a class="nav-link collapsed" data-bs-target="#checkin-nav" data-bs-toggle="collapse" href="#">
 				<i class="bi bi-check"></i><span>Checkin</span><i class="bi bi-chevron-down ms-auto"></i>
@@ -48,21 +63,30 @@
 					<a href="http://cafeguymalary.atalou.org/views/admin/checkin.php"><i class="bi bi-circle"></i><span>Go to Cafeteria</span></a>
 				</li>
 			</ul>
-		</li><!-- End section employe  -->
+		</li>
+		<!-- End section chekin  -->
 
-		<li class="nav-item">
-			<a class="nav-link collapsed" data-bs-target="#reports-nav" data-bs-toggle="collapse" href="#">
-				<i class="bi bi-journal-album"></i><span>Report</span><i class="bi bi-chevron-down ms-auto"></i>
-			</a>
-			<ul id="reports-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/dailyrep.php"><i class="bi bi-circle"></i><span>Daily Report</span></a>
+		<!-- Start section report -->
+		<?php
+		 if($_SESSION['admin']['id_role'] == 1):
+		?>
+				<li class="nav-item">
+					<a class="nav-link collapsed" data-bs-target="#reports-nav" data-bs-toggle="collapse" href="#">
+						<i class="bi bi-journal-album"></i><span>Report</span><i class="bi bi-chevron-down ms-auto"></i>
+					</a>
+					<ul id="reports-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/dailyrep.php"><i class="bi bi-circle"></i><span>Daily Report</span></a>
+						</li>
+						<li>
+							<a href="http://cafeguymalary.atalou.org/views/admin/globalrep.php"><i class="bi bi-circle"></i><span>Global Report</span></a>
+						</li>
+					</ul>
 				</li>
-				<li>
-					<a href="http://cafeguymalary.atalou.org/views/admin/globalrep.php"><i class="bi bi-circle"></i><span>Global Report</span></a>
-				</li>
-			</ul>
-		</li><!-- End Charts Nav -->
+		<?php
+		 endif;
+		?>
+		<!-- End section report -->
 
 		<li class="nav-item">
 			<a class="nav-link collapsed" data-bs-target="#apk-nav" data-bs-toggle="collapse" href="#">
